4cc7bfd2f20b4fb95c06c23f5bd27915a19ef62c
Rohit <2022kucp1029@iiitkota.ac.in>
2025-05-19 10:31:26 +0530

    update version

---
+++ main.go
@@ -0,0 +1,33 @@
+import main
+
+import(
+	"github.com/Ecom-go/controllers"
+	"github.com/Ecom-go/database"
+	"github.com/Ecom-go/middleware"
+	"github.com/Ecom-go/routes"
+	"github.com/Ecom-go/models"
+	"github.com/Ecom-go/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func main(){
+	port := os.Getenv("PORT")
+	if port == ""{
+		port = "8000"
+	}
+	
+	app := controllers.NewApplication(database.ProductData(database.Client, "Products"),database.UserData(database.Client,"Users"))
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	router.Get("/addtocart",app.AddToCart())
+	router.Get("/removeitem",app.RemoveItem())
+	router.Get("/cartcheckout",app.BuyFromCart())
+	router.Get("/instantbuy",app.InstantBuy())
+
+	log.Fatal(router.Run(":"+port))
+}
